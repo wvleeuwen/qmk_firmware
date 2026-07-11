b44b884b2e286640361efe0e311131e3d05803f2
Wijnand van Leeuwen <wvleeuwen@gmail.com>
2026-07-10 21:43:07 -0700

    Remove copyright notice from my own code and add some whitespace

--- keyboards/ploopyco/madromys/keymaps/daily/keymap.c
+++ keyboards/ploopyco/madromys/keymaps/daily/keymap.c
@@ -1,20 +1,3 @@
-/* Copyright 2023 Colin Lam (Ploopy Corporation)
- * Copyright 2020 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
- * Copyright 2019 Sunjun Kim
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 #include QMK_KEYBOARD_H
 
 extern bool is_drag_scroll;
@@ -43,12 +26,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 if (TIMER_DIFF_16(record->event.time, scroll_key_timer) < TAPPING_TERM) {
                     scroll_toggled = !scroll_toggled;
                 }
+
                 if (!scroll_toggled) {
                     set_drag_scroll(false);
                 }
             }
+
             return false;
     }
+
     return true;
 }
 
